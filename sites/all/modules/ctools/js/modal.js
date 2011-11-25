0cb0181703c8126469d4622a1aa660706f587870
Vladimir Semenov <svowl@cdev.ru>
2011-11-25 20:16:33 +0400

    [*] Drupal modules updated: Ctools, Views

--- sites/all/modules/ctools/js/modal.js
+++ sites/all/modules/ctools/js/modal.js
@@ -1,4 +1,3 @@
-// $Id: modal.js,v 1.27 2010/12/31 22:27:02 merlinofchaos Exp $
 /**
  * @file
  *
@@ -245,10 +244,6 @@
       $('#modal-content form:not(.ctools-use-modal-processed)', context)
         .addClass('ctools-use-modal-processed')
         .each(function() {
-          $('input[type=submit], button', this).click(function() {
-            this.form.clk = this;
-          });
-
           var element_settings = {};
 
           element_settings.url = $(this).attr('action');
@@ -258,6 +253,12 @@
 
           Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
           Drupal.ajax[base].form = $(this);
+
+          $('input[type=submit], button', this).click(function() {
+            Drupal.ajax[base].element = this;
+            this.form.clk = this;
+          });
+
         });
     }
   };
@@ -268,6 +269,9 @@
    * AJAX responder command to place HTML within the modal.
    */
   Drupal.CTools.Modal.modal_display = function(ajax, response, status) {
+    if ($('#modalContent').length == 0) {
+      Drupal.CTools.Modal.show(Drupal.CTools.Modal.getSettings(ajax.element));
+    }
     $('#modal-title').html(response.title);
     $('#modal-content').html(response.output);
     Drupal.attachBehaviors();
@@ -383,6 +387,14 @@
       } else { //IE
         event = window.event;
         target = event.srcElement;
+      }
+
+      var parents = $(target).parents().get();
+      for (var i in $(target).parents().get()) {
+        var position = $(parents[i]).css('position');
+        if (position == 'absolute' || position == 'fixed') {
+          return true;
+        }
       }
       if( $(target).filter('*:visible').parents('#modalContent').size()) {
         // allow the event only if target is a visible child node of #modalContent
